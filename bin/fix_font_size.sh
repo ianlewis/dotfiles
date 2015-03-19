8537a0e6cdc2f27d4d1d5682207347502c59d376
Ian Lewis <ianmlewis@gmail.com>
2015-03-19 21:08:29 +0900

    Updated fix_font_size.sh to make fonts bigger.

--- bin/fix_font_size.sh
+++ bin/fix_font_size.sh
@@ -6,6 +6,6 @@
 
 # Sleep so that this runs after whatever is messing up font sizes.
 sleep 5
-gsettings set com.ubuntu.user-interface scale-factor "{'eDP1': 10}"
+gsettings set com.ubuntu.user-interface scale-factor "{'eDP1': 12}"
 gsettings set com.canonical.Unity.Interface text-scale-factor '1.0'
-gsettings set org.gnome.desktop.interface text-scaling-factor '1.0'
+gsettings set org.gnome.desktop.interface text-scaling-factor '1.5'
