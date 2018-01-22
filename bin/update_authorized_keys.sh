22000a9ca7ce108691698756a9006ae40ac620ae
Ian Lewis <ianmlewis@gmail.com>
2018-01-22 23:28:35 +0900

    make shellcheck happy

--- bin/update_authorized_keys.sh
+++ bin/update_authorized_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p $HOME/.ssh
+mkdir -p "${HOME}/.ssh"
 KEYS=$(curl -s https://github.com/ianlewis.keys)
 if [ -n "$KEYS" ]; then
-    echo "$KEYS" > $HOME/.ssh/authorized_keys2
+    echo "$KEYS" > "${HOME}/.ssh/authorized_keys2"
 fi
