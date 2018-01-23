543697aa09965e11e658eb5cfa4cfb7414e3ce9f
Ian Lewis <ianmlewis@gmail.com>
2018-01-23 11:49:52 +0900

    Update authorized_keys only with new keys

--- bin/update_authorized_keys.sh
+++ bin/update_authorized_keys.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Adds public certs from Github to authorized_keys if not already added.
 mkdir -p "${HOME}/.ssh"
-KEYS=$(curl -s https://github.com/ianlewis.keys)
-if [ -n "$KEYS" ]; then
-    echo "$KEYS" > "${HOME}/.ssh/authorized_keys2"
+if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
+    touch "${HOME}/.ssh/authorized_keys"
 fi
+curl -s https://github.com/ianlewis.keys | while read -r key; do
+    grep -qF "$key" "${HOME}/.ssh/authorized_keys" || echo "$key" >> "${HOME}/.ssh/authorized_keys"
+done
