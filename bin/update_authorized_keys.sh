80bb1d000904d79fd8c276f3fda402e0bb3f0289
Ian Lewis <ianmlewis@gmail.com>
2018-03-17 12:30:15 +0900

    Remove keys that have been removed from Github

--- bin/update_authorized_keys.sh
+++ bin/update_authorized_keys.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# Adds public certs from Github to authorized_keys if not already added.
+# Adds public certs from Github to authorized_keys
 mkdir -p "${HOME}/.ssh"
 if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
     touch "${HOME}/.ssh/authorized_keys"
 fi
-curl -s https://github.com/ianlewis.keys | while read -r key; do
-    grep -qF "$key" "${HOME}/.ssh/authorized_keys" || echo "$key" >> "${HOME}/.ssh/authorized_keys"
-done
+
+# Only update authorized_keys if we can contact github. We want
+# to avoid writing an empty authorized_keys file if github.com is
+# inaccessible.
+TEMPFILE=$(mktemp)
+if curl -s https://github.com/ianlewis.keys > "${TEMPFILE}"; then
+    cat "${TEMPFILE}" > "${HOME}/.ssh/authorized_keys"
+fi
+
+# Adds local authorized_keys if not already there
+if [ -f "${HOME}/.ssh/authorized_keys.local" ]; then
+    while read -r key; do
+        grep -qF "$key" "${HOME}/.ssh/authorized_keys" || echo "$key" >> "${HOME}/.ssh/authorized_keys"
+    done < "${HOME}/.ssh/authorized_keys.local"
+fi
