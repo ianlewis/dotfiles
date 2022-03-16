35d72775935b7304e7017139145614135163a0ae
Ian Lewis <ianmlewis@gmail.com>
2022-03-16 10:47:15 +0900

    Fix shellcheck issues

--- test.sh
+++ test.sh
@@ -5,18 +5,18 @@ set -o pipefail
 ERRORS=()
 
 # find all executables and run `shellcheck`
-for f in $(find . -path ./vim/bundle -prune -o -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
-	if file "$f" | cut -d':' -f2 | grep --quiet shell; then
-		{
-			shellcheck -x "$f" && echo "[OK]: sucessfully linted $f"
-		} || {
-			# add to errors
-			ERRORS+=("$f")
-		}
-	fi
+for f in $(find . -path ./bash/lib -prune -o -path ./vim/bundle -prune -o -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
+    if file "$f" | cut -d':' -f2 | grep --quiet shell; then
+        {
+            shellcheck -x "$f" && echo "[OK]: sucessfully linted $f"
+        } || {
+            # add to errors
+            ERRORS+=("$f")
+        }
+    fi
 done
 
 if [ ${#ERRORS[@]} -ne 0 ]; then
-	echo "These files failed shellcheck: ${ERRORS[*]}"
-	exit 1
+    echo "These files failed shellcheck: ${ERRORS[*]}"
+    exit 1
 fi
