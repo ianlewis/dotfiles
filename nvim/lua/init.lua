b35c9f1c2d9b8c7ee6889c7b41927f7e22810568
Ian Lewis <ian@ianlewis.org>
2025-04-01 08:32:06 +0000

    feat: Add nvim-treesitter
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -12,6 +12,10 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+--{{{ package configs
+require("treesitter")
+--}}}
+
 -- {{{ nvim-surround
 local surround = require("nvim-surround")
 surround.setup()
