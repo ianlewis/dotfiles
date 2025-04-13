a415e0916298e01f00b484a71f4a19af3d6bf1d3
Ian Lewis <ian@ianlewis.org>
2025-04-13 12:49:30 +0900

    feat: Add sbp (#151)
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- nvim/ftplugin/gomod.lua
+++ nvim/ftplugin/gomod.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- gomod-specific options. 
+-- gomod-specific options.
 
 -- Use tabs.
 vim.opt.expandtab = false
