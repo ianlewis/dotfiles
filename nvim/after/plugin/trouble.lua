16635f16708665f19918a50aa8c10e739642a5bd
Ian Lewis <ian@ianlewis.org>
2025-08-23 08:11:38 +0000

    feat: add trouble error diagnostics filter
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -15,7 +15,7 @@
 local trouble = require("trouble")
 trouble.setup({
 	modes = {
-		test = {
+		all_diagnostics = {
 			mode = "diagnostics",
 			preview = {
 				type = "split",
@@ -24,17 +24,49 @@ trouble.setup({
 				size = 0.3,
 			},
 		},
+		error_diagnostics = {
+			mode = "diagnostics",
+			preview = {
+				type = "split",
+				relative = "win",
+				position = "right",
+				size = 0.3,
+			},
+			filter = {
+				any = {
+					{ severity = vim.diagnostic.severity.ERROR },
+					{ severity = vim.diagnostic.severity.WARN },
+				},
+			},
+		},
 	},
 })
 
-vim.keymap.set("n", "<leader>xx", "<cmd>Trouble test toggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xX", "<cmd>Trouble test toggle filter.buf=0<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", { silent = true, noremap = true })
-vim.keymap.set(
-	"n",
-	"<leader>cl",
-	"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-	{ silent = true, noremap = true }
-)
-vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble all_diagnostics toggle<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>xX", "<cmd>Trouble all_diagnostics toggle filter.buf=0<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", {
+	silent = true,
+	noremap = true,
+})
+vim.keymap.set("n", "<leader>xe", "<cmd>Trouble error_diagnostics toggle<cr>", {
+	silent = true,
+	noremap = true,
+})
