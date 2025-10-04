50f1f1cfe39d5756994e04523edc22272efe17fc
Ian Lewis <ian@ianlewis.org>
2025-10-04 01:28:26 +0000

    chore: disable nvim remote plugin providers
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- nvim/lua/ianlewis/globals.lua
+++ nvim/lua/ianlewis/globals.lua
@@ -27,4 +27,12 @@ do
 		"shell=sh",
 		"typescript",
 	}
+
+	-- Disable unnecessary language providers.
+	-- These providers allow the use of remote plugins written in other
+	-- languages but I don't use any that require them.
+	vim.g.loaded_node_provider = 0
+	vim.g.loaded_perl_provider = 0
+	vim.g.loaded_python3_provider = 0
+	vim.g.loaded_ruby_provider = 0
 end
