f2cca059b5c143c4fd1f71c3419dbd4b0ef2ed37
Ian Lewis <ian@ianlewis.org>
2026-06-09 03:43:13 +0000

    refactor(nvim): save osc52 import to local var
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- nvim/lua/ianlewis/options.lua
+++ nvim/lua/ianlewis/options.lua
@@ -60,15 +60,17 @@ vim.opt.colorcolumn = "+1"
 
 -- If running over SSH, override the clipboard provider to use OSC52
 if vim.env.SSH_TTY then
+	local osc52 = require("vim.ui.clipboard.osc52")
+
 	vim.g.clipboard = {
 		name = "OSC 52",
 		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+			["+"] = osc52.copy("+"),
+			["*"] = osc52.copy("*"),
 		},
 		paste = {
-			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+			["+"] = osc52.paste("+"),
+			["*"] = osc52.paste("*"),
 		},
 	}
 end
