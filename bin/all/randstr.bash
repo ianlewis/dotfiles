27da5e521edb8ab05336b30b9239c4c5734bed27
Ian Lewis <ian@ianlewis.org>
2026-06-12 06:21:32 +0000

    chore(randstr): update copyright
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- bin/all/randstr.bash
+++ bin/all/randstr.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: set ft=bash:
 #
-# Copyright 2025 Ian Lewis
+# Copyright 2026 Ian Lewis
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
