dc3d53ae678412d1b63c7b20df9cf7e881ef9a18
Ian Lewis <ian@ianlewis.org>
2026-06-24 07:19:16 +0000

    feat(clone): add --fork option
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- bin/all/clone.bash
+++ bin/all/clone.bash
@@ -21,6 +21,16 @@
 
 set -euo pipefail
 
+function _validate_repo_id() {
+    local repo_id="$1"
+
+    # Validate the repository ID format.
+    if [[ ! "${repo_id}" =~ ^[a-zA-Z0-9._-]+(/[a-zA-Z0-9._-]+)?$ ]]; then
+        echo "$(basename "${0}"): ERROR: Invalid repository '${repo_id}'. Must be in the format of <owner>/<repo> or <repo>." >&2
+        exit 1
+    fi
+}
+
 function _clone() {
     # These variables are used by the argsparse library.
     local program_params
@@ -37,12 +47,20 @@ function _clone() {
 
     argsparse_use_option "dir" "Project source directory." "value" "default:${HOME}/src" "short:d" "type:directory"
 
-    argsparse_use_option "new" "Create and clone new repository." "short:n"
-    argsparse_use_option "add-readme" "Add a README file to the new repository." "require:new"
+    # New repository options.
+    argsparse_use_option "new" "Create and clone new repository." "short:n" "exclude:fork"
     argsparse_use_option "gitignore" "Specify a gitignore template for the new repository." "value" "short:g" "require:new"
     argsparse_use_option "license" "Specify an Open Source License for the new repository." "value" "short:l" "require:new"
-    argsparse_use_option "private" "Make the new repository private." "require:new"
+    argsparse_use_option "add-readme" "Add a README file to the new repository." "require:new"
     argsparse_use_option "template" "Create a new repository from template." "value" "short:p" "require:new"
+    argsparse_use_option "private" "Make the new repository private." "require:new"
+
+    # Options for creating a new fork.
+    argsparse_use_option "fork" "Fork and clone a repository." "short:f" "exclude:new"
+    argsparse_use_option "fork-name" "" "value" "require:fork"
+
+    # --new or --fork is required.
+    argsparse_use_option "private" "Make the new repository or fork private." "require:new"
 
     # Command is optional and could contain many parts.
     argsparse_describe_parameters "REPOSITORY"
@@ -58,10 +76,30 @@ function _clone() {
 
     local repo_id
     local owner
+    local login_name
     local repo_name
     local dest
 
     repo_id="${program_params[0]}"
+
+    _validate_repo_id "${repo_id}"
+
+    owner="${repo_id%%/*}"
+    repo_name="${repo_id##*/}"
+
+    # Check that the GitHub CLI is installed.
+    if ! command -v gh >/dev/null 2>&1; then
+        echo "$(basename "${0}"): ERROR: GitHub CLI is not installed." >&2
+        exit 1
+    fi
+
+    # Check if the user is authenticated with GitHub CLI.
+    # NOTE: If not authenticated gh repo clone might check out the repository
+    # using HTTPS which would make you unable to push.
+    if ! login_name=$(gh auth status --json hosts | jq -r '.hosts."github.com"[0].login'); then
+        echo "$(basename "${0}"): ERROR: You are not logged into any GitHub hosts. To log in, set the GH_TOKEN environment variable or run: gh auth login." >&2
+        exit 1
+    fi
 
     # Create the repository if requested.
     if argsparse_is_option_set "new"; then
@@ -94,24 +132,43 @@ function _clone() {
         "${create_command[@]}"
     fi
 
-    # Check that the GitHub CLI is installed.
-    if ! command -v gh >/dev/null 2>&1; then
-        echo "$(basename "${0}"): ERROR: GitHub CLI is not installed." >&2
-        exit 1
-    fi
+    # Fork the repository if requested.
+    if argsparse_is_option_set "fork"; then
+        local fork_command
+        local fork_owner
+        local fork_repo_name
 
-    # Check if the user is authenticated with GitHub CLI.
-    # NOTE: If not authenticated gh repo clone might check out the repository
-    # using HTTPS which would make you unable to push.
-    if ! gh auth status >/dev/null 2>&1; then
-        echo "$(basename "${0}"): ERROR: You are not logged into any GitHub hosts. To log in, set the GH_TOKEN environment variable or run: gh auth login." >&2
-        exit 1
+        fork_command=(gh repo fork "${repo_id}" "--clone=false")
+
+        if argsparse_is_option_set "fork-name"; then
+            _validate_repo_id "${program_options['fork-name']}"
+
+            fork_owner="${program_options['fork-name']%%/*}"
+            fork_repo_name="${program_options['fork-name']##*/}"
+
+            # If the values are the same then the user only specified the fork
+            # repository name. We will assume the fork owner is the same as the
+            # login name.
+            if [ "${fork_owner}" == "${fork_repo_name}" ]; then
+                fork_owner="${login_name}"
+            fi
+
+            fork_command+=("--fork-name" "${fork_repo_name}")
+            if [ "${fork_owner}" != "${login_name}" ]; then
+                fork_command+=("--org" "${fork_owner}")
+            fi
+        else
+            fork_owner="${login_name}"
+            fork_repo_name="${repo_name}"
+        fi
+
+        "${fork_command[@]}"
+
+        owner="${fork_owner}"
+        repo_name="${fork_repo_name}"
     fi
 
     # Clone the repository.
-    owner=$(gh repo view "${repo_id}" --json owner | jq -r '.owner.login')
-    repo_name=$(gh repo view "${repo_id}" --json name | jq -r '.name')
-
     dest="${program_options['dir']}/${owner}/${repo_name}"
 
     if [ -e "${dest}" ]; then
